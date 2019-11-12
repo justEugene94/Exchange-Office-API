af3ce46a6c5a131a9bd935be9173356743a60ff7
Eugene <drydude77@gmail.com>
2019-11-12 17:38:42 +0200

    Added Api/CoefficientsController@show

--- app/Http/Controllers/Api/CoefficientsController.php
+++ app/Http/Controllers/Api/CoefficientsController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\Api\Coefficient\StoreFormRequest;
 use App\Http\Resources\Api\CoefficientResource;
 use App\Models\Coefficient;
+use App\Models\User;
 use App\Services\CoefficientService;
 use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Http\Request;
@@ -22,6 +23,7 @@ class CoefficientsController extends Controller
      */
     public function index()
     {
+        /** @var User $user */
         $user = Auth::user();
         $this->authorize('list', [Coefficient::class, $user]);
 
@@ -45,11 +47,14 @@ class CoefficientsController extends Controller
      */
     public function store(StoreFormRequest $request, CoefficientService $coefficientService)
     {
+        /** @var User $user */
         $user = Auth::user();
         $this->authorize('store', [Coefficient::class, $user]);
 
+        /** @var Coefficient $coefficient */
         $coefficient = $coefficientService->create($request);
 
+        /** @var CoefficientResource $resource */
         $resource = CoefficientResource::make($coefficient);
 
         return $resource;
@@ -58,12 +63,23 @@ class CoefficientsController extends Controller
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @param int $coefficientId
+     * @return CoefficientResource
+     * @throws AuthorizationException
      */
-    public function show(int $id)
+    public function show(int $coefficientId)
     {
-        //
+        /** @var User $user */
+        $user = Auth::user();
+        $this->authorize('show', [Coefficient::class, $user]);
+
+        /** @var Coefficient $coefficient */
+        $coefficient = Coefficient::query()->with('commerceValue')->findOrFail($coefficientId);
+
+        /** @var CoefficientResource $resource */
+        $resource = CoefficientResource::make($coefficient);
+
+        return $resource;
     }
 
     /**
