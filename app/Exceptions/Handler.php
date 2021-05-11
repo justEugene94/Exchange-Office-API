9295f06186b9d3a34c00995da7f2cd841b0f31a0
justEugene94 <drydude77@gmail.com>
2021-05-11 22:25:19 +0300

    Added json Exceptions

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,8 +3,12 @@
 namespace App\Exceptions;
 
 use Exception;
+use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
-use Symfony\Component\Routing\Exception\RouteNotFoundException;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Response;
+use Illuminate\Validation\ValidationException;
+use Throwable;
 
 class Handler extends ExceptionHandler
 {
@@ -39,25 +43,92 @@ class Handler extends ExceptionHandler
     }
 
     /**
-     * Render an exception into an HTTP response.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Exception  $exception
-     * @return \Illuminate\Http\Response
+     * @param \Illuminate\Http\Request $request
+     * @param \Exception $e
+     * @return \Illuminate\Http\JsonResponse|\Illuminate\Http\Response|\Symfony\Component\HttpFoundation\Response
+     * @throws Throwable
      */
-    public function render($request, Exception $exception)
+    public function render($request, Exception $e)
     {
-        if ($exception instanceof RouteNotFoundException) {
-            return response()->json(
-                [
-                    'errors' => [
-                        'status' => 401,
-                        'message' => 'Unauthenticated',
-                    ]
-                ], 401
-            );
+
+        $e = $this->prepareException($e);
+
+        if ($e instanceof AuthenticationException) {
+            return $this->unauthenticated($request, $e);
         }
 
-        return parent::render($request, $exception);
+        if ($e instanceof ValidationException) {
+            return $this->convertValidationExceptionToResponse($e, $request);
+        }
+
+        return $this->prepareResponse($request, $e);
+    }
+
+    /**
+     * @param \Illuminate\Http\Request $request
+     * @param AuthenticationException $exception
+     * @return \Illuminate\Http\JsonResponse
+     */
+    protected function unauthenticated($request, AuthenticationException $exception): JsonResponse
+    {
+        return JsonResponse::create([
+            'result' => [
+                'status' => 'error',
+                'message' => $exception->getMessage(),
+            ],
+        ], Response::HTTP_UNAUTHORIZED);
+    }
+
+    /**
+     * Create a response object from the given validation exception.
+     *
+     * @param  \Illuminate\Validation\ValidationException  $e
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    protected function convertValidationExceptionToResponse(ValidationException $e, $request): JsonResponse
+    {
+        return JsonResponse::create([
+            'result' => [
+                'status' => 'error',
+                'message' => $e->getMessage(),
+                'validation' => $e->errors()
+            ],
+        ], Response::HTTP_UNPROCESSABLE_ENTITY);
+    }
+
+    /**
+     * @param \Illuminate\Http\Request $request
+     * @param Exception $e
+     *
+     * @return JsonResponse|Response|\Symfony\Component\HttpFoundation\Response
+     */
+    protected function prepareResponse($request, Exception $e)
+    {
+        $statusCode   = $this->isHttpException($e) ? $e->getStatusCode() : 500;
+
+        return JsonResponse::create([
+            'result' => [
+                'status' => 'error',
+                'message' => $e->getMessage(),
+                'trace' => $this->getBacktrace($e)
+            ],
+        ], $statusCode);
+    }
+
+    /**
+     * @param Exception $e
+     *
+     * @return array
+     */
+    protected function getBacktrace(Exception $e): array
+    {
+        $backtrace = [];
+
+        if (config('app.debug')) {
+            $backtrace = $this->convertExceptionToArray($e);
+        }
+
+        return $backtrace;
     }
 }
